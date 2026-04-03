c5d4525d04e36b11b32ec93913fb56545d87a647
Haripriya-p1702 <haripriyasvb2018@gmail.com>
2026-04-03 15:03:45 +0530

    Add code

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return Hello from GitOps Sentinel Hackathon demo!"
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=80000000)
+        app.run(host="0.0.0.0", port=8000000)
